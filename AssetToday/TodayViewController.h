dc13ab33b3452d81e70fbd566bffdd77d27b3991
Ben Chatelain <benchatelain@gmail.com>
2015-02-24 13:17:06 -0700

    Add Today extension target

---
+++ AssetToday/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  AssetToday
+//
+//  Created by Ben Chatelain on 2/24/15.
+//  Copyright (c) 2015 Radar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
