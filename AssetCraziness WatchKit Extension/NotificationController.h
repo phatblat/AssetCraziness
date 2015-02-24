1aa0a4d46355add3f0d2f3cbf2c7689da4d50ab5
Ben Chatelain <benchatelain@gmail.com>
2015-02-24 13:19:06 -0700

    Add watchkit extension target

---
+++ AssetCraziness WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  AssetCraziness WatchKit Extension
+//
+//  Created by Ben Chatelain on 2/24/15.
+//  Copyright (c) 2015 Radar. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
