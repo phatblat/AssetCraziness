1272fe62628b90edce361c2dc6c710a11b192299
Ben Chatelain <benchatelain@gmail.com>
2015-02-24 13:14:15 -0700

    Initial Commit

---
+++ AssetCraziness/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AssetCraziness
+//
+//  Created by Ben Chatelain on 2/24/15.
+//  Copyright (c) 2015 Radar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
